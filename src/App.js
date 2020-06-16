7ce0b59459f782bca5bb72cfb31871e42a28728d
wagnerflores <60112509+wagnerflores11@users.noreply.github.com>
2020-06-16 15:16:33 -0300

    tela de usuário pronta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-
 import './App.css';
+
+import logo from './img/logo2.png';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello word</h1>
+
+    <div className='container'>
+      <img src={logo} alt='Trex' />
+
+
+      <div className="content">
+        <p><strong>DIVIDENDO </strong>cadastro de dividas
+      </p>
+
+      <form>
+        <label htmlFor='email'>E-MAIL *</label>
+        <input type='email' id='email' placeholder='Seu melhor e-mail'/>
+        <button className= "btn" type='submit'>Entrar</button>
+      </form>
+
+      </div>
     </div>
+
   );
 }
 
